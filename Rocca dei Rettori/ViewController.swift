34fce621a86e95d224b9d87a92e6c58662c7d5cc
Denis Bystruev <dbystruev@me.com>
2018-06-18 00:40:05 +0300

    Initial Commit

---
+++ Rocca dei Rettori/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rocca dei Rettori
+//
+//  Created by Denis Bystruev on 18/06/2018.
+//  Copyright © 2018 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
