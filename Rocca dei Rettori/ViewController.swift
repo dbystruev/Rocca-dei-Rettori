abf5676819b7508befcbf797f3dc6b57662b96ac
Denis Bystruev <dbystruev@me.com>
2018-06-18 10:03:15 +0300

    Allow user to move pin to its current location by long pressing on the label

--- Rocca dei Rettori/ViewController.swift
+++ Rocca dei Rettori/ViewController.swift
@@ -10,13 +10,17 @@ import CoreLocation
 import MapKit
 import UIKit
 
-class ViewController: UIViewController, UIGestureRecognizerDelegate {
+class ViewController: UIViewController, UIGestureRecognizerDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var mapView: MKMapView!
     
     /// Rocca dei Rettori's coordinate
     let center = CLLocationCoordinate2D(latitude: 41.129437, longitude: 14.782375)
+    
+    
+    /// Manager for location related events
+    var locationManager: CLLocationManager! = nil
     
     /// Point to save our pin
     var pin: MKPointAnnotation!
@@ -26,9 +30,6 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     
     /// Double gesture recognizer for the map view
     var doubleTapGesture: UITapGestureRecognizer!
-    
-    /// Long press gesture recognizer for the map viewe
-    var longPressGesture: UILongPressGestureRecognizer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -67,21 +68,29 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     
     /// Add tap gesture recognizers
     func addGestureRecognizers() {
-        // add single tap gesture recognizer
+        // add single tap gesture recognizer for map view
         tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapHandler(_:)))
         tapGesture.delegate = self
         mapView.addGestureRecognizer(tapGesture)
         
-        // add double tap gesture recognizer
+        // add double tap gesture recognizer for map view
         doubleTapGesture = UITapGestureRecognizer(target: self, action: #selector(tapHandler(_:)))
         tapGesture.delegate = self
         tapGesture.numberOfTapsRequired = 2
         mapView.addGestureRecognizer(doubleTapGesture)
         
-        // add long press handler
-        longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(longPressHandler(_:)))
-        longPressGesture.delegate = self
-        mapView.addGestureRecognizer(longPressGesture)
+        // add long press handler for map view
+        let longPressMap = UILongPressGestureRecognizer(target: self, action: #selector(longPressMapHandler(_:)))
+        longPressMap.delegate = self
+        mapView.addGestureRecognizer(longPressMap)
+        
+        // add long press handler for label
+        let longPressLabel = UILongPressGestureRecognizer(target: self, action: #selector(longPressLabelHandler(_:)))
+        longPressLabel.delegate = self
+        label.addGestureRecognizer(longPressLabel)
+        
+        // enable user interactions for label
+        label.isUserInteractionEnabled = true
     }
     
     
@@ -116,10 +125,10 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     }
     
     
-    /// Handle long press gesture
+    /// Handle long press gesture for map
     ///
     /// - Parameter gestureRecognizer: UILongPressGestureRecognizer passed to us by Cocoa Touch
-    @objc func longPressHandler(_ gestureRecognizer: UILongPressGestureRecognizer) {
+    @objc func longPressMapHandler(_ gestureRecognizer: UILongPressGestureRecognizer) {
         // get the location of tap on screen
         let location = gestureRecognizer.location(in: self.mapView)
         
@@ -134,6 +143,15 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     }
     
     
+    /// Handle long press gesture for label
+    ///
+    /// - Parameter gestureRecognizer: UILongPressGestureRecognizer passed to us by Cocoa Touch
+    @objc func longPressLabelHandler(_ gestureRecognizer: UILongPressGestureRecognizer) {
+        // start tracking the location and move the pin there
+        findCurrentLocation()
+    }
+    
+    
     // Don't recognize a single tap until a double tap fails
     // see https://developer.apple.com/documentation/uikit/touches_presses_and_gestures/coordinating_multiple_gesture_recognizers/preferring_one_gesture_over_another
     func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRequireFailureOf otherGestureRecognizer: UIGestureRecognizer) -> Bool {
@@ -142,6 +160,56 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     
     func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldBeRequiredToFailBy otherGestureRecognizer: UIGestureRecognizer) -> Bool {
         return gestureRecognizer == tapGesture && otherGestureRecognizer == doubleTapGesture
+    }
+    
+    
+    /// Find out current location
+    func findCurrentLocation() {
+        // check if location manager is used for the first time
+        if locationManager == nil {
+            // if nil need to setup location manager
+            locationManager = CLLocationManager()
+            
+            // set the delegate
+            locationManager.delegate = self
+            
+            // set the best accuracy
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            
+            // request permission to use location services while the app is running
+            locationManager.requestAlwaysAuthorization()
+        }
+        
+        // check if location services are enabled
+        if CLLocationManager.locationServicesEnabled() {
+            // start tracking the location
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    
+    // Called when location is found out
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // stop updating user location until the next long press on the label
+        manager.stopUpdatingLocation()
+        
+        // get the user location
+        let location = locations[0]
+        
+        // get the corresponding 2D coordinates on the map
+        let coordinate = location.coordinate
+    
+        // get the current span
+        let span = mapView.region.span
+        
+        // define a new region with the center in the new location
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        
+        // move the map to the new region
+        mapView.setRegion(region, animated: true)
+        
+        // drop a pin at the new location
+        setPin(coordinate)
     }
     
     
